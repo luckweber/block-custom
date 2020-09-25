e56f3fdf3734bd91f61ae62b4ea0b7ee273e31f9
unknown <thiago.pereira@agencian1.com.br>
2020-09-25 15:27:05 -0300

    Fixed Vbase

--- react/SearchSeller.tsx
+++ react/SearchSeller.tsx
@@ -61,7 +61,10 @@ const SearchSelleritem = ({ data: { name, image, cellPhone } }: any) => {
                 <p>{cellPhone}</p>
             </div>
             <div className="w-10 flex flex items-center">
-                <svg style={{ width: '20px' }} data-v-bdd6b5ac="" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className=" arrow-profile text-magalu-color svg-inline--fa fa-chevron-right fa-w-10"><path data-v-bdd6b5ac="" fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" className=""></path></svg></div>
+                <a href={`https://api.whatsapp.com/send?phone=${cellPhone}&text=Oi, sou ${name}.`}>
+                    <svg style={{ width: '20px' }} data-v-bdd6b5ac="" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className=" arrow-profile text-magalu-color svg-inline--fa fa-chevron-right fa-w-10"><path data-v-bdd6b5ac="" fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" className=""></path></svg>
+                </a>
+            </div>
             <hr className="w-100" style={{ color: 'red', border: "1px solid rgba(0,0,0,.1)" }} />
         </div >
     )
